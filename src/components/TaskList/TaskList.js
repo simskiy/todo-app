f6c0a9e0c57dc6ecf237f6d9fd45351519f8b95c
simskiy <simskiy@gmail.com>
2022-09-07 17:49:15 +0300

    переделал передачу параметров из родительского компонента (app) в дочерний (TaskList)

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,11 +2,22 @@ import React from "react";
 import Task from "../Task";
 
 const TaskList = ({params}) => {
+  const taskArr = params.map(item => {
+    const {id, className, ...opt} = item
+    const inputElement = className ? <input type="text" className="edit" defaultValue={"Editing task"} /> : null
+    return (
+      <li key={id} className={className}>
+        <Task
+          options={opt}
+        />
+        {inputElement}
+      </li>
+    )
+  })
+
   return (
     <ul className="todo-list">
-      <Task options={params.completed}/>
-      <Task options={params.editing} />
-      <Task options={params.active} />
+      {taskArr}
     </ul>
   )
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, {Component} from "react";
 import NewTaskForm from "../NewTaskForm";
 import Main from "../Main";
 import './app.css'
-import params from './params'
+// import params from './params'
 
-const App = () => {
-    return (
-      <section className="todoapp">
-        <NewTaskForm />
-        <Main params={params}/>
-      </section>
-    )
+export default class App extends Component {
+  state = {
+    params: [
+      {
+        className: "completed",
+        descriptionText: 'Completed task',
+        createdText: 'created 17 seconds',
+        id: 1
+      },
+      {
+        className: 'editing',
+        descriptionText: 'Editing task',
+        createdText: 'created 5 minutes ago',
+        id: 2
+      },
+      {
+        type: null,
+        descriptionText: 'Active task',
+        createdText: 'created 5 minutes ago',
+        id: 3
+      }
+    ]
+  }
+    render() {
+      return (
+        <section className="todoapp">
+          <NewTaskForm />
+          <Main params={this.state.params}/>
+        </section>
+      )
+    }
 }
-
-export default App
-
