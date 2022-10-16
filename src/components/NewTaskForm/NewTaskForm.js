66b53f4e2482afc92926566bf9fc478531ae432c
simskiy <simskiy@gmail.com>
2022-10-16 18:29:21 +0300

    Переделал немного компоненты под новые ТЗ (теперь таски будут с таймером). Пока только разметка и стили без логики.

--- src/components/NewTaskForm/NewTaskForm.js
+++ src/components/NewTaskForm/NewTaskForm.js
@@ -4,17 +4,21 @@ const NewTaskForm = ({ onCreateTask }) => {
   return (
     <header className="header">
       <h1>todos</h1>
-      <input
-        className="new-todo"
-        placeholder="What needs to be done?"
-        autoFocus
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            if (e.target.value.trim()) onCreateTask(e.target.value)
-            e.target.value = ''
-          }
-        }}
-      />
+      <form className="new-todo-form">
+        <input
+          className="new-todo"
+          placeholder="Task"
+          autoFocus
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              if (e.target.value.trim()) onCreateTask(e.target.value)
+              e.target.value = ''
+            }
+          }}
+        />
+        <input className="new-todo-form__timer" placeholder="Min" autoFocus />
+        <input className="new-todo-form__timer" placeholder="Sec" autoFocus />
+      </form>
     </header>
   )
 }
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -10,8 +10,13 @@ export default class Task extends Component {
       <div className="view">
         <input className="toggle" type="checkbox" onClick={onCompleted} />
         <label>
-          <span className="description">{descriptionText}</span>
-          <span className="created"> {date} ago</span>
+          <span className="title">{descriptionText}</span>
+          <span className="description">
+            <button className="icon icon-play"></button>
+            <button className="icon icon-pause"></button>
+            12:25
+          </span>
+          <span className="description"> {date} ago</span>
         </label>
         <button className="icon icon-edit" onClick={onEditStart} />
         <button className="icon icon-destroy" onClick={onDelete} />
@@ -19,3 +24,15 @@ export default class Task extends Component {
     )
   }
 }
+
+/*
+<label>
+  <span class="title">fw</span>
+  <span class="description">
+    <button class="icon icon-play"></button>
+    <button class="icon icon-pause"></button>
+    12:25
+  </span>
+  <span class="description">created 17 seconds ago</span>
+</label>
+*/
